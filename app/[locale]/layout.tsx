c619fb2ba0d77490d6bc94d4c8934ca332fcc727
carvz0r <jarv.aleksei@gmail.com>
2025-11-17 16:45:53 +0200

    applied dark theme and better font layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import {NextIntlClientProvider} from 'next-intl';
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { NextIntlClientProvider } from "next-intl";
+import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
   title: "Aripaev Test Work",
@@ -25,10 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head/>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="antialiased font-sans text-gray-700 dark:text-gray-300"
       >
-         <NextIntlClientProvider>{children}</NextIntlClientProvider>
+        <ThemeProvider attribute="class" defaultTheme="system">
+          <NextIntlClientProvider>{children}</NextIntlClientProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,8 +6,8 @@ import LocaleSwitcher from "@/components/LocaleSwitcher";
 export default function Home() {
   const t = useTranslations("HomePage");
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans text-gray-700 dark:text-gray-300" style={{background: 'var(--background)'}}>
-      <main className="flex min-h-screen w-full md:max-w-5xl flex-col items-center sm:items-start shadow-xl shadow-gray-200 gap-16 px-8 py-10 md:py-20 md:px-16" style={{background: 'var(--card)', boxShadow: "0 20px 25px -5px var(--shadow)"}}>
+    <div className="flex min-h-screen items-center justify-center" style={{background: 'var(--background)'}}>
+      <main className="flex min-h-screen w-full md:max-w-5xl flex-col items-center sm:items-start gap-16 px-8 py-10 md:py-20 md:px-16" style={{background: 'var(--card)', boxShadow: "0 0 25px -5px var(--shadow)"}}>
         <div className="flex xs:flex-col sm:flex-col md:flex-row items-center md:justify-between w-full gap-8">
           <TypographyH1>🇪🇪 {t("title")}</TypographyH1>
           <LocaleSwitcher />
