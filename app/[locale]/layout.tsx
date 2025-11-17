b3e709ec95c8f7b4e7d8ac899b2f1a8b1f783282
carvz0r <jarv.aleksei@gmail.com>
2025-11-17 20:19:34 +0200

    define locale for the html tag and add toaster

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,26 +1,38 @@
 import type { Metadata } from "next";
 import "../globals.css";
-import { NextIntlClientProvider } from "next-intl";
+import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+import { setRequestLocale } from "next-intl/server";
 
 export const metadata: Metadata = {
   title: "Aripaev Test Work",
   description: "Salary Calculator",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+type Props = {
   children: React.ReactNode;
-}>) {
+  params: Promise<{ locale: string }>;
+};
+
+export default async function RootLayout({ children, params }: Props) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+  setRequestLocale(locale);
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head/>
+    <html lang={locale} suppressHydrationWarning>
+      <head />
       <body
         className="antialiased font-sans text-gray-700 dark:text-gray-300"
+        suppressHydrationWarning
       >
         <ThemeProvider attribute="class" defaultTheme="system">
           <NextIntlClientProvider>{children}</NextIntlClientProvider>
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
